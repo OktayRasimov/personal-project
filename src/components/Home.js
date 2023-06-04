9480cc3e489e123d64ef9298028c88609be44343
Oktay Rasimov <oktayrasimov123@gmail.com>
2023-06-04 22:15:32 +0300

    the beginning

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import "./Home.css";
+const Home = () => {
+  return (
+    <div>
+      <h1>Front-end Web Developer</h1>
+      <p>
+        Hi,i'm Oktay Rasimov.A passionate Front-end Developer based in
+        Asenovgrad,Plovdiv
+      </p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/MainHeader.js
@@ -0,0 +1,17 @@
+import "./MainHeader.css";
+
+const MainHeader = () => {
+  return (
+    <div className="main-header">
+      <h2>Oktay.dev</h2>
+      <div>
+        <a>Home</a>
+        <a>About</a>
+        <a>Projects</a>
+        <a>Contact</a>
+      </div>
+    </div>
+  );
+};
+
+export default MainHeader;
