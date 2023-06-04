ee1601dac3aa2a47df4a9ccf7fec435821b06cb9
Oktay Rasimov <oktayrasimov123@gmail.com>
2023-06-05 00:30:50 +0300

    added background mainly and fixes to it

---
+++ src/components/Section1.js
@@ -0,0 +1,14 @@
+import "./Section1.css";
+import MainHeader from "./MainHeader";
+import Home from "./Home";
+
+const Section1 = () => {
+  return (
+    <div className="section-first">
+      <MainHeader />
+      <Home />
+    </div>
+  );
+};
+
+export default Section1;
