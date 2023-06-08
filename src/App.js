d04750f04c02cfff7608b9988f47197b6151782a
Oktay Rasimov <oktayrasimov123@gmail.com>
2023-06-08 16:02:30 +0300

    added scrollbar style

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Home from "./components/Home";
 import Section1 from "./components/Section1";
 import Section2 from "./components/Section2/Section2";
 import Section4 from "./components/Section4/Section4";
+
+import "simplebar-react/dist/simplebar.min.css";
+
 function App() {
   return (
     <div className="App">
